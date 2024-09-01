e838c196a2c840049e9f2a379c1e0b23596c186e
Lucián Blažek <lucian@ftmo.com>
2024-09-01 22:48:28 +0200

    fix: update DI

--- DI/ServiceLocator/BaseServiceInitializer.cs
+++ DI/ServiceLocator/BaseServiceInitializer.cs
@@ -1,20 +1,32 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace ClownMeister.UnityEssentials.DI.ServiceLocator
 {
     public abstract class BaseServiceInitializer : MonoBehaviour
     {
         private static bool _servicesInitialized = false;
+        [SerializeField]
+        private List<GameObject> persistentObjects = new List<GameObject>();
 
         private void Awake()
         {
             if (_servicesInitialized) return;
             RegisterServices();
+
+            // Make all specified persistent objects persistent.
+            // Most likely the one that holds all the services and this initializer
+            foreach (GameObject obj in persistentObjects)
+            {
+                DontDestroyOnLoad(obj);
+            }
+
             _servicesInitialized = true;
             OnServicesLoad();
         }
 
         // Child classes must implement this method to register their specific services
+        // Use ServiceLocator.Register<>()
         protected abstract void RegisterServices();
 
         // Child classes must implement this method to customize what happens after services are loaded
