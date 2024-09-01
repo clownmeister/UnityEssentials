cfe027d64e2267ce7c854bb0cb65667b8e52755c
Lucián Blažek <lucian@ftmo.com>
2024-09-01 22:41:47 +0200

    feat: add DI ServiceLocator and BaseServiceInitializer

---
+++ DI/ServiceLocator/BaseServiceInitializer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace ClownMeister.UnityEssentials.DI.ServiceLocator
+{
+    public abstract class BaseServiceInitializer : MonoBehaviour
+    {
+        private static bool _servicesInitialized = false;
+
+        private void Awake()
+        {
+            if (_servicesInitialized) return;
+            RegisterServices();
+            _servicesInitialized = true;
+            OnServicesLoad();
+        }
+
+        // Child classes must implement this method to register their specific services
+        protected abstract void RegisterServices();
+
+        // Child classes must implement this method to customize what happens after services are loaded
+        protected abstract void OnServicesLoad();
+    }
+}
---
+++ DI/ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClownMeister.UnityEssentials.DI.ServiceLocator
+{
+    public static class ServiceLocator
+    {
+        private static readonly Dictionary<Type, object> Services = new();
+
+        public static T Resolve<T>()
+        {
+            try
+            {
+                return (T)Services[typeof(T)];
+            }
+            catch (KeyNotFoundException)
+            {
+                throw new ApplicationException($"Service of type {typeof(T)} not found.");
+            }
+        }
+
+        public static void Register<T>(T service)
+        {
+            if (Services.ContainsKey(typeof(T)))
+            {
+                throw new ApplicationException($"Service of type {typeof(T)} is already registered.");
+            }
+
+            Services[typeof(T)] = service;
+        }
+
+        public static void Unregister<T>()
+        {
+            if (!Services.ContainsKey(typeof(T)))
+            {
+                throw new ApplicationException($"Service of type {typeof(T)} is not registered. Can't unregister.");
+            }
+
+            Services.Remove(typeof(T));
+        }
+    }
+}
